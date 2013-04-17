9dfd2a9e45e5d3a2765ead1f760aa113f6822105
Alessandro Franceschi <al@lab42.it>
2013-04-17 19:05:52 +0200

    Added sample configfile define for single files creation

---
+++ manifests/configfile.pp
@@ -0,0 +1,48 @@
+#
+# = Define: stdmod::configfile
+#
+# The define manages stdmod configfile
+#
+#
+# == Parameters
+#
+# [*ensure*]
+#   String. Default: present
+#   Manages configfile presence. Possible values:
+#   * 'present' - Install package, ensure files are present.
+#   * 'absent' - Stop service and remove package and managed files
+#
+# [*template*]
+#   String. Default: Provided by the module.
+#   Sets the path of a custom template to use as content of configfile
+#   If defined, configfile file has: content => content("$template")
+#   Example: template => 'site/configfile.conf.erb',
+#
+# [*options*]
+#   Hash. Default undef. Needs: 'template'.
+#   An hash of custom options to be used in templates to manage any key pairs of
+#   arbitrary settings.
+#
+define stdmod::configfile (
+  $ensure   = present,
+  $template = 'stdmod/configfile.erb' ,
+  $options  = '' ,
+  $ensure  = present ) {
+
+  include stdmod
+
+  file { "stdmod_configfile_${name}":
+    ensure  => $ensure,
+    path    => "${stdmod::dir_path}/${name}",
+    mode    => $stdmod::file_mode,
+    owner   => $stdmod::file_owner,
+    group   => $stdmod::file_group,
+    require => Package[$stdmod::package],
+    notify  => $stdmod::file_notify,
+    content => template($template),
+    replace => $stdmod::file_replace,
+    audit   => $stdmod::file_audit,
+    noop    => $stdmod::noops,
+  }
+
+}
